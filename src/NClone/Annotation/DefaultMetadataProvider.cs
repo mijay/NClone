64710d928aa658c9e6fdc2006673d42606469784
Dmitry Kononchuk <kononchukdmitry@gmail.com>
2013-08-26 01:21:22 +0200

    default MetadataProvider implemented

---
+++ src/NClone/Annotation/DefaultMetadataProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace NClone.Annotation
+{
+    /// <summary>
+    /// Default implementation of <see cref="IMetadataProvider"/>.
+    /// </summary>
+    public class DefaultMetadataProvider: IMetadataProvider
+    {
+        public IEnumerable<FieldInfo> GetReplicatingMembers(Type entityType)
+        {
+            return entityType
+                .GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+        }
+    }
+}

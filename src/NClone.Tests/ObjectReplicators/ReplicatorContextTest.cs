b6777f08929885a00d336543d8efa9d4dff8ac23
Dmitry Kononchuk <kononchukdmitry@gmail.com>
2013-09-25 23:55:59 +0200

    Implement ReplicationContext + tests

---
+++ src/NClone.Tests/ObjectReplicators/ReplicatorContextTest.cs
@@ -0,0 +1,73 @@
+﻿using FakeItEasy;
+using FakeItEasy.ExtensionSyntax.Full;
+using NClone.ObjectReplicators;
+using NClone.ReplicationStrategies;
+using NUnit.Framework;
+
+namespace NClone.Tests.ObjectReplicators
+{
+    public class ReplicatorContextTest: TestBase
+    {
+        private IReplicationStrategyFactory dummyFactory;
+
+        protected override void SetUp()
+        {
+            base.SetUp();
+            dummyFactory = A.Fake<IReplicationStrategyFactory>(x => x.Strict());
+        }
+
+        private static IReplicationStrategyFactory FactoryForReplicatorThat(Class onReceiving, Class returns)
+        {
+            var strategy = A.Fake<IReplicationStrategy>(x => x.Strict());
+            strategy
+                .CallsTo(x => x.Replicate(onReceiving, A<IReplicationContext>.Ignored))
+                .Returns(returns);
+            var strategyFactory = A.Fake<IReplicationStrategyFactory>(x => x.Strict());
+            strategyFactory
+                .CallsTo(x => x.StrategyForType(typeof (Class)))
+                .Returns(strategy);
+            return strategyFactory;
+        }
+
+        [Test]
+        public void SourceIsNull_NullReturned()
+        {
+            object result = new ReplicationContext(dummyFactory).Replicate(null);
+
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void SourceIsNotNull_Replicated()
+        {
+            var source = new Class();
+            var sourceReplica = new Class();
+            IReplicationStrategyFactory strategyFactory = FactoryForReplicatorThat(onReceiving: source, returns: sourceReplica);
+            var replicationContext = new ReplicationContext(strategyFactory);
+
+            object result = replicationContext.Replicate(source);
+
+            Assert.That(result, Is.SameAs(sourceReplica));
+        }
+
+        [Test]
+        public void ReplicateSameSourceTwice_FactoryIsCalledOnlyOnce()
+        {
+            var source = new Class();
+            var sourceReplica = new Class();
+            IReplicationStrategyFactory strategyFactory = FactoryForReplicatorThat(onReceiving: source, returns: sourceReplica);
+            var replicationContext = new ReplicationContext(strategyFactory);
+
+            replicationContext.Replicate(source);
+            replicationContext.Replicate(source);
+
+            strategyFactory
+                .StrategyForType(typeof (Class))
+                .CallsTo(x => x.Replicate(null, null))
+                .WithAnyArguments()
+                .MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        private class Class { }
+    }
+}
---
+++ src/NClone/ObjectReplicators/ReplicationContext.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using NClone.ReplicationStrategies;
+
+namespace NClone.ObjectReplicators
+{
+    /// <summary>
+    /// Implementation of <see cref="IReplicationContext"/>.
+    /// </summary>
+    internal class ReplicationContext: IReplicationContext
+    {
+        private readonly IDictionary<object, object> replicatedEntries = new Dictionary<object, object>();
+        private readonly IReplicationStrategyFactory replicationStrategyFactory;
+
+        public ReplicationContext(IReplicationStrategyFactory replicationStrategyFactory)
+        {
+            this.replicationStrategyFactory = replicationStrategyFactory;
+        }
+
+        public object Replicate(object source)
+        {
+            if (source == null)
+                return null;
+
+            object result;
+            if (replicatedEntries.TryGetValue(source, out result))
+                return result;
+
+            IReplicationStrategy replicationStrategy = replicationStrategyFactory.StrategyForType(source.GetType());
+            result = replicationStrategy.Replicate(source, this);
+
+            replicatedEntries.Add(source, result);
+            return result;
+        }
+    }
+}

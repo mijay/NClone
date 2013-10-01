41a29f2bdc84eedf854954cfeaca8b1273a78478
Dmitry Kononchuk <kononchukdmitry@gmail.com>
2013-10-01 12:53:33 +0200

    Changed documentation a bit

--- src/NClone/MetadataProviders/ConventionalMetadataProvider.cs
+++ src/NClone/MetadataProviders/ConventionalMetadataProvider.cs
@@ -9,10 +9,16 @@ namespace NClone.MetadataProviders
     /// and information from <see cref="CustomReplicationBehaviorAttribute"/>s.
     /// </summary>
     /// <remarks>
-    /// <para>Used conventions:</para>
-    /// <para>1) All structures are immutable => use <see cref="ReplicationBehavior.Copy"/> behavior.</para>
-    /// <para>2) All <see cref="Delegate"/>s are not copied during replication.</para>
-    /// <para>3) Lazy <see cref="IEnumerable"/> are illegal inside replicating types (causes exception).</para>
+    /// <para>Used conventions:
+    /// <list type="bullet">
+    /// <item>All structures are immutable => use <see cref="ReplicationBehavior.Copy"/> behavior.</item>
+    /// <item>All <see cref="Delegate"/>s are not copied during replication.</item>
+    /// <item>Lazy <see cref="IEnumerable"/> are illegal inside replicating types (causes exception).</item>
+    /// </list>
+    /// </para>
+    /// 
+    /// <para>If you want to override conventional behavior for some type or member, just apply
+    /// <see cref="CustomReplicationBehaviorAttribute"/> to it.</para>
     /// </remarks>
     /// <seealso cref="AttributeBasedMetadataProvider"/>
     /// <seealso cref="CustomReplicationBehaviorAttribute"/>

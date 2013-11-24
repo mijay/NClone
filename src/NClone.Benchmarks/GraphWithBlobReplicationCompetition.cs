7a9549ff944cf9bcf87605abe8220320293418d4
Dmitry Kononchuk <kononchukdmitry@gmail.com>
2013-11-24 20:50:41 +0100

    One more benchmark to test how NClone process arrays of objects

---
+++ src/NClone.Benchmarks/GraphWithBlobReplicationCompetition.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using GeorgeCloney;
+using NClone.Benchmarks.Runner;
+using NClone.MetadataProviders;
+using NClone.ObjectReplication;
+
+namespace NClone.Benchmarks
+{
+    public class GraphWithBlobReplicationCompetition: CompetitionBase
+    {
+        private const int blobSize = 200000;
+
+        private static void Consume(SomeClass source)
+        {
+            source.Property.Sum(x => x.field.Length);
+        }
+
+        [Benchmark]
+        public Action ReplicatorClone()
+        {
+            var source = new SomeClass
+                         {
+                             Property = Enumerable.Range(0, blobSize)
+                                 .Select(i => new SomeClass2 { field = i.ToString() })
+                                 .ToArray()
+                         };
+
+            var replicator = new ObjectReplicator(new ConventionalMetadataProvider());
+
+            return () => {
+                       source = replicator.Replicate(source);
+                       Consume(source);
+                   };
+        }
+
+        [Benchmark]
+        public Action ReflectionClone()
+        {
+            var source = new SomeClass
+                         {
+                             Property = Enumerable.Range(0, blobSize)
+                                 .Select(i => new SomeClass2 { field = i.ToString() })
+                                 .ToArray()
+                         };
+
+            return () => {
+                       source = source.DeepClone();
+                       Consume(source);
+                   };
+        }
+
+        private class SomeClass
+        {
+            public SomeClass2[] Property { get; set; }
+        }
+
+        private class SomeClass2
+        {
+            public string field;
+        }
+    }
+}

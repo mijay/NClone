7730b444f27560c55644cd58415030c6dd22f64f
Dmitry Kononchuk <kononchukdmitry@gmail.com>
2013-12-02 23:49:43 +0100

    Additional unit test

---
+++ src/NClone.Tests/ReplicationStrategies/CopyArrayReplicationStrategyTest.cs
@@ -0,0 +1,39 @@
+﻿using FakeItEasy;
+using mijay.Utils;
+using NClone.ObjectReplication;
+using NClone.ReplicationStrategies;
+using NUnit.Framework;
+
+namespace NClone.Tests.ReplicationStrategies
+{
+    public class CopyArrayReplicationStrategyTest: TestBase
+    {
+        private readonly IReplicationContext dummyContext = A.Fake<IReplicationContext>(c => c.Strict());
+
+        [Test]
+        public void SourceArrayContainsElements_NewArrayWithSameElementsReturned()
+        {
+            var source = new[] { new SomeClass(), new SomeClass() };
+
+            var result = CopyArrayReplicationStrategy.Instance.Replicate(source, dummyContext).As<SomeClass[]>();
+
+            Assert.That(result, Is.Not.SameAs(source));
+            Assert.That(result, Is.EqualTo(source));
+        }
+
+        [Test]
+        public void SourceArrayIsEmpty_NewEmptyArrayReturned()
+        {
+            var source = new SomeClass[0];
+
+            var result = CopyArrayReplicationStrategy.Instance.Replicate(source, dummyContext).As<SomeClass[]>();
+
+            Assert.That(result, Is.Not.SameAs(source));
+            Assert.That(result, Is.Empty);
+        }
+
+        private class SomeClass
+        {
+        }
+    }
+}

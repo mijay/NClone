36cbc5a21b0ae87d20b076d81a71aca13b01fd7b
Mitya Kononchuk <mitya.kononchuk@mendix.com>
2015-10-09 17:54:09 +0200

    Fix easy-to-fix tests

--- src/NClone/ReplicationStrategies/CloneArrayReplicationStrategy.cs
+++ src/NClone/ReplicationStrategies/CloneArrayReplicationStrategy.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using mijay.Utils;
 using NClone.MemberAccess;
 using NClone.ObjectReplication;
@@ -28,10 +29,13 @@ namespace NClone.ReplicationStrategies
                 "This replicator can copy only arrays of elements of type {0}, but {1} received",
                 elementType, source.GetType().GetElementType());
             var resultingArray = source.As<Array>().Clone().As<Array>();
-            for (int i = resultingArray.Length - 1; i >= 0; i--) {
+            for (int i = resultingArray.Length - 1; i >= 0; i--)
+            {
                 object sourceElement = getElement(resultingArray, i);
-                object resultingElement = context.ReplicateAsync(sourceElement);
-                setElement(resultingArray, i, resultingElement);
+                var _i = i;
+                context.ReplicateAsync(sourceElement)
+                    .ContinueWith(t => setElement(resultingArray, _i, t.Result),
+                        TaskContinuationOptions.ExecuteSynchronously);
             }
             return resultingArray;
         }

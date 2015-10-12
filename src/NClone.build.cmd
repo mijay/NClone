c98a67a3e5f52193a518d0fa4a1bb6a759c438af
Mitya Kononchuk <mitya.kononchuk@mendix.com>
2015-10-12 10:13:28 +0200

    Add support for NET46 compilation target, but remove NET35

--- src/NClone.build.cmd
+++ src/NClone.build.cmd
@@ -3,7 +3,7 @@ setlocal
 set MsBuild="%SystemRoot:"=%\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 set NuGet="packages\NuGet.CommandLine.2.8.6\tools\NuGet.exe"
 
+%MsBuild% NClone.sln /t:Clean,Build /m:2 /p:Configuration=NET46
 %MsBuild% NClone.sln /t:Clean,Build /m:2 /p:Configuration=NET45
 %MsBuild% NClone.sln /t:Clean,Build /m:2 /p:Configuration=NET40
-%MsBuild% NClone.sln /t:Clean,Build /m:2 /p:Configuration=NET35
 %NuGet% pack NClone.nuspec

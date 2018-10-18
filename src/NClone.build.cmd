5f6f2eda56d94da8ede97487339b211c0b618435
Mitya Kononchuk <mitya.kononchuk@mendix.com>
2018-10-18 09:47:59 +0200

    Remove support for .net 3.5 as it doesn't have ConcurrentDictionary

--- src/NClone.build.cmd
+++ src/NClone.build.cmd
@@ -5,5 +5,4 @@ set NuGet="packages\NuGet.CommandLine.2.8.6\tools\NuGet.exe"
 
 %MsBuild% NClone.sln /t:Clean,Build /m:2 /p:Configuration=NET45
 %MsBuild% NClone.sln /t:Clean,Build /m:2 /p:Configuration=NET40
-%MsBuild% NClone.sln /t:Clean,Build /m:2 /p:Configuration=NET35
 %NuGet% pack NClone.nuspec
